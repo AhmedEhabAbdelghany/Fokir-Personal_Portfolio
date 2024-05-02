481147da595f84c3a22c4b07f18f04227d6e8fac
Ahmed Ehab Abdelghany <102571403+AhmedEhabAbdelghany@users.noreply.github.com>
2024-05-03 01:56:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed-text', {
+    strings: ["Designer", "", "Developer"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+    startDelay: 2000,
+    // showCursor: false // Hide the default cursor
+    // backSpeed: 50
+});
